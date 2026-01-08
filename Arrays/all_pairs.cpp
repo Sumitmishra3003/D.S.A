7ea53b7b383235aa3bc1073ad9b787de027e9546
Sumitmishra3003 <sumitind272001@gmail.com>
2026-01-08 13:12:22 +0000

    array

---
+++ Arrays/all_pairs.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+ using namespace std;
+
+ int main(){
+
+    int arr[] = {1,2,3};
+    int n = 3;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i+1; j++){
+            cout<<arr[i]<<arr[j]<<" ";
+        }
+        cout<<endl;
+    }
+ }
