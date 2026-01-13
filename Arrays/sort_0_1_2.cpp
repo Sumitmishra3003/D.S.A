7e369a91f0bdf0a2caaaa4bd80369ab1f51fa5dd
Sumitmishra3003 <sumitind272001@gmail.com>
2026-01-13 13:36:16 +0000

    array

--- Arrays/sort_0_1_2.cpp
+++ Arrays/sort_0_1_2.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 
 void sorting(int arr[], int size){
@@ -41,31 +41,65 @@ int main(){
     }
 
     sorting(arr, size);
-}
-
-
-// dutch natinal flag algorithm.
-
-/*void sort012(int arr[], int size){
-    int low = 0, mid = 0, high = size - 1;
-
-    while(mid <= high){
-        if(arr[mid] == 0){
-            swap(arr[low], arr[mid]);
-            low++;
-            mid++;
-        }
-        else if(arr[mid] == 1){
-            mid++;
-        }
-        else{ // arr[mid] == 2
-            swap(arr[mid], arr[high]);
-            high--;
-        }
-    }
 }*/
-
-
 // time complexity - O(n)
 // space complexity - O(1)
 // in both the solution, time and space complexity is same
+
+// but the above code only print the array it does not modify the actual array.
+
+#include<iostream>
+using namespace std;
+
+void sorting(int arr[], int size){
+    int zerocount = 0;
+    int onecount = 0;
+    int twocount = 0;
+
+    for(int i=0; i<size; i++)
+{    if(arr[i] == 0)
+    zerocount++;
+
+    if(arr[i] == 1)
+    onecount++;
+
+    if(arr[i] == 2)
+    twocount++;
+}
+
+int i = 0;
+
+   while (zerocount--){
+     arr[i] = 0;
+      i++;
+    }
+   while (onecount--) {
+    arr[i] = 1;
+     i++;
+    }
+   while (twocount--) {
+    arr[i] = 2;
+     i++;
+    }
+
+   for(int i=0; i<size; i++){
+    cout<<arr[i];
+   }
+}
+
+int main(){
+
+    int arr[1000];
+    int size;
+
+    cout<<"enter size of array : ";
+    cin>>size;
+
+    for(int i=0; i<size; i++){
+        cout<<"enter element at index "<<i<<" : ";
+        cin>>arr[i];
+    }
+
+    sorting(arr, size);
+}
+// this code actually modify the code
